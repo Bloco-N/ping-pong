5853f0fec2f845ff07fafc940e3fc5c56d60bde7
ilwel <ilwelisaac@hotmail.com>
2022-01-31 16:02:10 -0300

    bot ping pong

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const Client = require('./Client');
+const { Intents } = require('discord.js');
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.once("ready", () => {
+    console.log("working");
+});
+
+client.on("message", (message) => {
+    if (message.content === "ping") {
+        message.channel.send("pong");
+    }
+})
+
+client.login(process.env.TOKEN_DA_MASSA);
+
+// https://discord.com/api/oauth2/authorize?client_id=937782777141682186&permissions=8&scope=bot
+
