d7682a14adfce29f07801a7983c391d24d341b22
ilwel <ilwelisaac@hotmail.com>
2022-01-31 22:11:52 -0300

    change to agostinho bot

---
+++ frases.js
@@ -0,0 +1,31 @@
+const frases = [
+    'É hora de esquecer os erros do passado e começar a planejar os erros do futuro.',
+    'Alguns relacionamentos são como uma partida de futebol, Em certas ocasiões aparece um melhor para lhe substituir',
+    'Como é que pode no “memo” dia o cara ser assaltado, sequestrado, assassinado e ainda acabar a gasolina do carro? ',
+    'Bebel, nós estamos brigando negócio de dinheiro, dinheiro é coisa material, o importante é que nós estamos bem, estamos com saúde.',
+    'Lineu, gostaria de agradecer você a essa crítica que você ai, extremamente destrutiva, realmente ajudou muito a levantar minha moral. Obrigado. ',
+    'Beiçola me vê dois pasteis de carne aí e bota na conta do seu floriano',
+    'Este arcondicionado é a garantia do meu casamento',
+    'Bebel, não vamo polemizar',
+    'Olha, Lineu, oportunidade não bate duas vezes na mesma porta. confia em mim!',
+    'Senti uma catinga achei que era o seu Floriano não falei nada pq não queria ofender né',
+    '"MEU quarto"? ta dizendo isso só porque a casa é do SEU pai??',
+    'Maria Isabel, tu já fez duas perguntas, eu ainda não fiz nenhuma',
+    'com esse calor danado aqui a bebel não me da nem bom dia',
+    'Izabel não tem nada a ver com pirâmide isso aí eh uma coisa chamada Jantar da Fortuna pelo amor de deus',
+    'Lineu você está insinuando que eu me aproveito de você?',
+    'seu Floriano... o senhor tem... 100 reais pra me emprestar??',
+    'Lineu!!! finalmente um homem de VISÃO nessa casa!',
+    'Carrara Taxi, ou Taxi Carrara a gnt ainda não decidiu ainda',
+    'a dona nenê a senhora pra mim é a pessoa mais especial que tem eu jamais botaria a senhora num esquema furado to botando num esquema que eu sei que da certo',
+    'VOU EMBORA DAQUI MARIA IZABEL',
+    'culpa MINHA?!! eu fiz dentro da honestidade denda LEI! eu não fiz nada de errado!',
+    'sim a senhora esta pagando o taxi mas não é a minha dona',
+    'devemos deixar de ser egoístas e começar a pensar em nós mesmos',
+    'antes de você... esculhambar... deixa eu falar duas outras coisas que eu gostaria de colocar aí pra toda família...',
+    'ô Lineu, você acha que eu ia deixar você numa fria rapaz?',
+    'seu Floriano... o senhor tem... 100 reais pra me emprestar??'
+
+]
+
+module.exports = frases;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const Client = require('./Client');
+const frases = require('./frases');
 const { Intents } = require('discord.js');
 const client = new Client({
     intents: [
@@ -13,17 +14,14 @@ client.once("ready", () => {
 });
 
 client.on("message", (message) => {
-    if (message.content === "ping") {
-        message.channel.send("pong");
-    }
 
-    if (message.content === "tomar no seu cu") {
-        message.channel.send("vai você seu fdp");
+    if (message.content === 'agostinho') {
+        message.channel.send(frases[Math.floor(Math.random() * (frases.length - 0)) + 0]);
     }
 
 })
 
 client.login(process.env.TOKEN_DA_MASSA);
 
-// https://discord.com/api/oauth2/authorize?client_id=937782777141682186&permissions=8&scope=bot
+// https://discord.com/api/oauth2/authorize?client_id=937782777141682186&permissions=2048&scope=bot
 
